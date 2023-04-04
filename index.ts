c704426f6ed8a36d7ecb672ba0126fffb2e91163
Bruno mac <brunob43@outlook.com>
2023-04-04 06:50:09 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,16 @@
+import express from "express"
+const app = express()
+app.use(express.json()) //MD que transforma las req.body a json
+
+const PORT = 3000
+
+app.get("/ping", (_req, res) => {
+    console.log("someone pinged here");
+    res.send("pong")
+    
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    
+})
