26f43887a923600a4429c76724350aa3bbdb02ec
Bruno mac <brunob43@outlook.com>
2023-04-04 12:00:46 -0300

    types.d.ts

---
+++ types.d.ts
@@ -0,0 +1,3 @@
+//este es un fichero que TS siempre mira y busca por defecto, aca estaran todos los tipados que queremos definir y uililizar
+
+export type Weather = "sunny" | "rainy" | "cloudy" | "windy" | "stormy"
