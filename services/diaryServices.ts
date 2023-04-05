6a3d83be4c8e9b58f67a78817666a5dbd8f0e387
Bruno mac <brunob43@outlook.com>
2023-04-05 19:16:06 -0300

    getEntries

--- services/diaryServices.ts
+++ services/diaryServices.ts
@@ -9,6 +9,14 @@ const diaries: DiaryEntry[] = diaryData as DiaryEntry[]
 
 export const getEntries = (): DiaryEntry[] => diaries
 
-export const getEntriesWithOutSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => diaries
-
+export const getEntriesWithoutSensitiveInfo = (): NonSensitiveInfoDiaryEntry[] => {
+  return diaries.map(({ id, date, weather, visibility }) => {
+    return {
+      id,
+      date,
+      weather,
+      visibility
+    }
+  })
+}
 export const addEntry = (): undefined => undefined
