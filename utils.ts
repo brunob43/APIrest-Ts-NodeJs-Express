15056a33d81c1b8982bc30515490badca9f84a89
Bruno mac <brunob43@outlook.com>
2023-04-07 07:47:29 -0300

    isString

--- utils.ts
+++ utils.ts
@@ -1,15 +1,19 @@
 import { NewDiaryEntry } from './types'
 
 const parseComment = (commentFromRequest: any): string => {
-  if (typeof commentFromRequest !== 'string') {
+  if (!isString(commentFromRequest)) {
     throw new Error('Incorrect or missing comment')
   }
   return commentFromRequest
 }
 
+const isString = (string: string): boolean => {
+  return typeof string === 'string'
+}
+
 const toNewDiaryEntry = (object: any): NewDiaryEntry => {
   const newEntry: NewDiaryEntry = {
-
+    comment: parseComment(object.comment)
   }
   return newEntry
 }
