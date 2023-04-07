bef145a37a75c25624da5f1641ac934a2025a8b5
Bruno mac <brunob43@outlook.com>
2023-04-07 07:57:37 -0300

    date

--- utils.ts
+++ utils.ts
@@ -7,13 +7,37 @@ const parseComment = (commentFromRequest: any): string => {
   return commentFromRequest
 }
 
+const parseDate = (dateFromRequest: any): string => {
+  if (!isString(dateFromRequest) || !isDate(dateFromRequest)) {
+    throw new Error('Incorrect or missing date')
+  }
+  return dateFromRequest
+}
+
+const parseWeather = (weatherFromRequest: any): string => {
+  if (!isString(weatherFromRequest) || !isDate(weatherFromRequest)) {
+    throw new Error('Incorrect or missing date')
+  }
+  return weatherFromRequest
+}
+
+// ------------------------------------------------------- 
+
 const isString = (string: string): boolean => {
   return typeof string === 'string'
 }
 
+const isDate = (date: string): boolean => {
+  return Boolean(Date.parse(date))
+}
+
+// -----------------------------------------------------------
+
 const toNewDiaryEntry = (object: any): NewDiaryEntry => {
   const newEntry: NewDiaryEntry = {
-    comment: parseComment(object.comment)
+    comment: parseComment(object.comment),
+    date: parseDate(object.date)
+
   }
   return newEntry
 }
