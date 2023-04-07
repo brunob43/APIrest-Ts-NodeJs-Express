612b607d1a21529829ef4bea438390807b30b336
Bruno mac <brunob43@outlook.com>
2023-04-07 07:34:21 -0300

    comments

--- utils.ts
+++ utils.ts
@@ -1,5 +1,12 @@
 import { NewDiaryEntry } from './types'
 
+const parseComment = (commentFromRequest: any): string => {
+  if (typeof commentFromRequest !== 'string') {
+    throw new Error('Incorrect or missing comment')
+  }
+  return commentFromRequest
+}
+
 const toNewDiaryEntry = (object: any): NewDiaryEntry => {
   const newEntry: NewDiaryEntry = {
 
